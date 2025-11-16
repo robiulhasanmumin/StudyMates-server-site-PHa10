32375e7b5098ed109afc380d6a89a8ad7f8ae707
robiulhasanmumin <ot7mumin@gmail.com>
2025-11-16 17:03:08 +0600

    Delete operation done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000;
 
 app.use(cors())
@@ -57,6 +57,13 @@ async function run() {
       const result = await connectionCollection.insertOne(newConnection)
       res.send(result)
 
+    })
+
+    app.delete("/connection/:id", async(req,res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await connectionCollection.deleteOne(query)
+      res.send(result)
     })
 
 
