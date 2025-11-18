ac17f54f879ff09769dd4c80adcedec0ce6ca2b8
robiulhasanmumin <ot7mumin@gmail.com>
2025-11-18 15:42:59 +0600

    curd operation done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,7 +8,9 @@ const port = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://studyMates:cmbpKoBwGcpgtBrU@cluster0.5tck6.mongodb.net/?appName=Cluster0`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5tck6.mongodb.net/?appName=Cluster0`;
+
 
 const client = new MongoClient(uri, {
   serverApi: {
